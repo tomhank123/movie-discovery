2dee18b848b58bf04726bbd3e2d587281f1613bd
Duc Tran Minh <ductm@devblock.net>
2021-06-12 21:37:54 +0700

    refactor: update ui

--- app/components/Collections/index.js
+++ app/components/Collections/index.js
@@ -9,7 +9,7 @@ import PropTypes from 'prop-types';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Skeleton from 'react-loading-skeleton';
 
-import Alert from 'react-bootstrap/Alert';
+import { Alert, Card, Button } from 'react-bootstrap';
 import Titles from 'components/Titles';
 import swiperOptions from './config';
 import MovieList from './MovieList';
@@ -17,8 +17,29 @@ import Wrapper from './Wrapper';
 
 function Collections({ loading, error, items, isSwiper = false }) {
   if (loading) {
+    const CardLoader = () => (
+      <Card className="border-0 shadow-sm">
+        <Skeleton
+          wrapper="div"
+          style={{
+            paddingTop: '150%',
+          }}
+        />
+        <Card.Body>
+          <div className="d-grid gap-2">
+            <Button variant="secondary" size="sm">
+              Watch Now
+            </Button>
+            <Button variant="outline-secondary" size="sm">
+              Watch List
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+    );
+
     return (
-      <React.Fragment>
+      <Wrapper>
         {[1, 2, 3, 4, 5, 6].map(id => (
           <section key={id}>
             <Skeleton wrapper="h5" width={250} />
@@ -26,20 +47,20 @@ function Collections({ loading, error, items, isSwiper = false }) {
               <Swiper {...swiperOptions}>
                 {[7, 8, 9, 10, 11, 12, 13, 14, 15].map(childId => (
                   <SwiperSlide key={childId}>
-                    <Skeleton wrapper="div" height={320} />
+                    <CardLoader />
                   </SwiperSlide>
                 ))}
               </Swiper>
             ) : (
               <MovieList>
                 {[7, 8, 9, 10, 11, 12, 13, 14, 15].map(childId => (
-                  <Skeleton key={childId} wrapper="div" height={320} />
+                  <CardLoader key={childId} />
                 ))}
               </MovieList>
             )}
           </section>
         ))}
-      </React.Fragment>
+      </Wrapper>
     );
   }
 
